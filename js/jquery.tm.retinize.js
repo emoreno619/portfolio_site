f928cc0c98636e5e8f8a84fe4504dc3d58cced58
Eduardo Moreno <emoreno@uchicago.edu>
2015-09-07 20:12:54 -0700

    includes parallax scroll with console window on mac image

--- js/jquery.tm.retinize.js
+++ js/jquery.tm.retinize.js
@@ -13,6 +13,31 @@
 
 	'use strict';
 
+	// BEGIN APPLE
+
+	var el = $('.intro'),
+	        mac = $('#mac'),
+	        offset = mac.offset(),
+	        windowHeight = $(window).height();
+
+	    $(window).on('scroll', function() {
+
+	        $('h1').fadeOut(500);
+
+	        var windowTop = $(window).scrollTop(),
+	            scrollPercent = (offset.top - windowTop) / offset.top,
+	            scale = 'scale(' + scrollPercent + ')';
+
+	        el.css('transform', scale);
+
+	        if (windowTop >= 940) {
+	            el.hide();
+	        } else {
+	            el.show();
+	        }
+	    });
+	// END APPLE
+
 	$.fn.retinizeImages = function ( options ) {
 		
 		// Defaults
